514f4154a6d9ab3a8e775db6cdd3fea13e39e350
Matias Rozanec <rozanecm@gmail.com>
2020-07-13 17:39:09 -0300

    respect debug flag to omit logging when not desired

--- rustico/src/logger/logger.rs
+++ rustico/src/logger/logger.rs
@@ -9,21 +9,17 @@ pub struct Logger {}
 
 impl Logger {
     pub fn new(should_i_log: bool, debug_msg_receiver: Receiver<String>) -> JoinHandle<()> {
-        println!("in log msgs!");
         let child = thread::spawn(move || {
-            println!("spawned thread");
             let mut file = File::create("log.rustico").expect("error opening log file");
             loop {
-                println!("in loop");
                 let current_msg = debug_msg_receiver.recv().expect("error reading");
-
-                file.write_all(current_msg.as_ref()).expect("error writing log file");
-                file.write_all("\n".as_ref()).expect("error writing log file");
+                if should_i_log {
+                    // println!("{}", format!("[DEBUG] {}", current_msg));
+                    file.write_all(current_msg.as_ref()).expect("error writing log file");
+                    file.write_all("\n".as_ref()).expect("error writing log file");
+                }
                 if current_msg.as_str() == STOP_LOGGING_MSG {
                     break;
-                }
-                if should_i_log {
-                    println!("{}", format!("[DEBUG] {}", current_msg));
                 }
             }
         });
